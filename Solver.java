00e2a064019c8037295191fce1627198a7c95485
Will <wylie1027@comcast.net>
2018-04-26 17:39:50 -0400

    4/26/18
    Tried to finish the solving part of project.

--- Solver.java
+++ Solver.java
@@ -6,10 +6,13 @@ public class Solver{
             for(int i = 0; i < boxes.length; i++){
                 boxes[i].rowPos = checkRow(boxes[i]);
                 boxes[i].colPos = checkCol(boxes[i]);
-                boxes[i].cellPos = checkCell(boxes[i]);
-                isOnlyNum(boxes[i].rowPos, boxes[i].colPos, boxes[i].cellPos);
+                boxes[i].cellPos = checkCell(boxes[i], boxes);
+                if(boxes[i].valueOfText != 0){
+                    isOnlyNum(boxes[i], boxes[i].rowPos, boxes[i].colPos, boxes[i].cellPos);
+                }
             }
         } while(!isSolved(boxes));
+        System.out.println("Completed");
     }
     public static int[] checkRow(Box box) {
         int[] output = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -23,14 +26,21 @@ public class Solver{
     public static int[] checkCol(Box box) {
         int[] output = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int i = box.col; i < 80 - (9 - box.col); i+=9) {
-            output[Main.boxes[i].valueOfText - 1] = 0;
+            if (Main.boxes[i].valueOfText != 0) {
+                output[Main.boxes[i].valueOfText - 1] = 0;
+            }
         }
         return output;
     }
-    public static int[] checkCell(Box box) { //TOMORRRROOOWOW
+    public static int[] checkCell(Box box, Box[] boxes) { //TOMORRRROOOWOW
         int[] output = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = box.index - box.col; i < box.index-box.col+9; i++) {
-            output[Main.boxes[i].valueOfText - 1] = 0;
+        int cellN = box.cellNum;
+        for (int i = 0; i < boxes.length; i++) {
+            if(boxes[i].cellNum == cellN){
+                if (Main.boxes[i].valueOfText != 0) {
+                    output[boxes[i].valueOfText - 1] = 0;
+                }
+            }
         }
         return output;
     }
@@ -44,7 +54,20 @@ public class Solver{
         }
         return out;
     }
-    public static void isOnlyNum(int[] row, int[] col, int[] cell){
-        
+    public static void isOnlyNum(Box box, int[] row, int[] col, int[] cell){
+        boolean firstFound = false, secondFound = false;
+        int index = -1;
+        for(int i = 0; i < row.length; i++){
+            if(row[i] == col[i] && col[i] == cell[i] && !firstFound){
+                firstFound = true;
+                index = i;
+            } else if(row[i] == col[i] && col[i] == cell[i] && !secondFound){
+                secondFound = true;
+                break;
+            }
+        }
+        if(firstFound && !secondFound && index != -1){
+            box.valueOfText = row[index];
+        }
     }
 }
